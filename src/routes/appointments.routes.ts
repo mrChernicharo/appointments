7f36e392c681192e2fe502e8fc95c39f0147ce3a
mrChernicharo <felipe.chernicharo@gmail.com>
2020-04-28 12:23:59 -0300

    rota post & app.use(express.json()) no server

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,26 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
-appointmentsRouter.get('/', (request, response) =>
-  response.json({ message: 'Hello Brother!' })
-);
+const appointments = [];
+
+appointmentsRouter.get('/', (request, response) => {
+  return response.json({ message: 'Hello Boy' });
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
 
 export default appointmentsRouter;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 // http://localhost:3333/appointments
-
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 
