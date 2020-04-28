58682f20854f7c45f5041bf0b58534b827cafd10
mrChernicharo <felipe.chernicharo@gmail.com>
2020-04-28 13:00:42 -0300

    Model => abstração da interfce

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,26 +1,19 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
 
-// 3. criamos a interface
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-
-const appointments: Appointment[] = []; // 4. Daí é informar o tipo dos elementos contidos no array
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  // 2. 'appointments' implicity has an 'any[]' type  ==> precisamos dos tipos
-  const findAppointmentInSameDate = appointments.find(
-    appointment => isEqual(parsedDate, appointment.date) // 1. percorrer cada appointment dentro do array aplicando a function isEqual
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date)
   );
 
   if (findAppointmentInSameDate) {
@@ -29,11 +22,15 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate, // 5. não esquece de ajeitar o nome "date" pra propriedade. parsedDate é apenas a variável processada na nossa lógica
-  };
+  // const appointment = {
+  //   id: uuid(),
+  //   provider,
+  //   date: parsedDate,
+  // };
+
+  // criação de novvo Appointment: os dados obtidos no request.body são passados pro constructor
+
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
