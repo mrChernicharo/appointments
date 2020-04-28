609ceffd2886eb654590c75f0828277c54b38414
mrChernicharo <felipe.chernicharo@gmail.com>
2020-04-28 12:53:02 -0300

    regra de negócio e interface de appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,21 +1,38 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+// 3. criamos a interface
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
-appointmentsRouter.get('/', (request, response) => {
-  return response.json({ message: 'Hello Boy' });
-});
+const appointments: Appointment[] = []; // 4. Daí é informar o tipo dos elementos contidos no array
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  // 2. 'appointments' implicity has an 'any[]' type  ==> precisamos dos tipos
+  const findAppointmentInSameDate = appointments.find(
+    appointment => isEqual(parsedDate, appointment.date) // 1. percorrer cada appointment dentro do array aplicando a function isEqual
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate, // 5. não esquece de ajeitar o nome "date" pra propriedade. parsedDate é apenas a variável processada na nossa lógica
   };
 
   appointments.push(appointment);
